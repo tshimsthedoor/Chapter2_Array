de6c09fad8be0eca848d1ce10d0daefacc98876e
tresor <tshims79@gmail.com>
2023-06-22 12:30:15 +0200

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names; // can reference any array of strings
+            names = new string[4];
+
+            // Storing items at index positions
+            names[0] = "Jakes";
+            names[1] = "Yvettes";
+            names[2] = "Pitshou";
+            names[3] = "Ida";
+        }
+    }
+}
